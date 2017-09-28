fa1e818fab76e5ae83bf92d648a2ed6301a83e2c
dbroqua <dbroqua@mousur.org>
2017-09-28 21:53:51 +0200

    - Fixed some bugs and added features

--- slice.sh
+++ slice.sh
@@ -1,7 +1,5 @@
 #! /bin/bash
 
-#TODO: support_material!
-
 #title              :slice.sh
 #description        :Just a "GUI" for slic3r
 #author             :Damien Broqua
@@ -18,6 +16,25 @@ PRINTER=""
 STLS=""
 SUPPORT=0
 ADD_OTHER_FILE=1
+SLICER_CONFIG='/tmp/slic3r.ini'
+SLICER_CMD=''
+
+function clean_tmp {
+    rm ${SLICER_CONFIG}
+}
+
+#Looking for slic3r
+SLICER_CMD=`which slic3r-prusa`
+if [ $? -eq 1 ] ; then
+    SLICER_CMD=`which slic3r-prusa3d`
+    if [ $? -eq 1 ] ; then
+        SLICER_CMD=`which slic3r`
+        if [ $? -eq 1 ] ; then
+            echo -e "\e[31mSlic3r not found\e[0m"
+            exit 1
+        fi
+    fi
+fi
 
 let i=0 # define counting variable
 W=() # define working array
@@ -28,7 +45,6 @@ while read -r line; do # process file by file
     W+=($i "$line")
 done < <( ls -1 ${PRINTERS_PATH} )
 PRINTER_ID=$(dialog --title "Choose printer" --menu "Printer" 24 80 17 "${W[@]}" 3>&2 2>&1 1>&3) # show dialog and store output
-clear
 
 if [ $? -ne 0 ]; then # Exit
     exit $?
@@ -43,13 +59,23 @@ if [ $? -eq 0 ]; then
     SUPPORT=1
 fi
 
+sed "s/support_material = 0/support_material = ${SUPPORT}/" ${PRINTER} > ${SLICER_CONFIG}
+
+
 # Select STL file
 W=()
+i=0
 while read -r line; do
     let i=$i+1
     W+=($i "$line")
 done < <( ls -1 ${STL_PATH}/*.{STL,stl} )
 
+if [ "${W}" == "" ] ; then
+    clear
+    clean_tmp
+    echo -e "\e[31mNo stl found in the current directory!\e[0m"
+    exit 2
+fi
 while [ ${ADD_OTHER_FILE} -eq 1 ] ; do
     STL_ID=$(dialog --title "Choose STL" --menu "STL (cancel to slice result)" 24 80 17 "${W[@]}" 3>&2 2>&1 1>&3)
     if [ $? -eq 1 ] ; then
@@ -62,7 +88,11 @@ done
 clear
 
 if [ "${STLS}" != "" ] ; then
-    slic3r-prusa3d --load ${PRINTER} -m ${STLS}
+    ${SLICER_CMD} --load ${SLICER_CONFIG} -m ${STLS}
+    clean_tmp
+    exit 0
 else
     echo "No file selected"
+    clean_tmp
+    exit 3
 fi
